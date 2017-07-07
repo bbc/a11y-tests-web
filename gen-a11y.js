ad31885698bf16cda278bb7a28b524e7892fc252
Andy Smith <andy.smith04@bbc.co.uk>
2017-07-07 09:41:55 +0100

    Add A11Y_PATHS env option for running certain pages rather than all of them

--- gen-a11y.js
+++ gen-a11y.js
@@ -1,14 +1,14 @@
 const fs = require('fs');
 const path = require('path');
 
-const pathsFile = 'paths.txt';
-const a11yJsFile = 'a11y.js';
+const PATHS_FILE = 'paths.txt';
+const A11Y_JS_FILE = 'a11y.js';
 
-const pathsContents = fs.readFileSync(pathsFile, { encoding: 'utf-8' });
+const pathsContents = getPathsContent();
 const paths = pathsContents.split('\n').filter((value) => value.trim() !== '');
 const a11yOutput = paths.reduce((acc, cur) => acc + pathToOutput(cur), '');
 
-fs.writeFileSync(a11yJsFile, a11yOutput);
+fs.writeFileSync(A11Y_JS_FILE, a11yOutput);
 
 function pathToOutput(path) {
   const baseUrl = process.env.A11Y_BASE_URL || "http://www.bbc.co.uk";
@@ -16,4 +16,8 @@ function pathToOutput(path) {
     page("${baseUrl}${path}", {
       hide: ['orb', 'bbccookies-prompt']
     })`;
+}
+
+function getPathsContent() {
+  return process.env.A11Y_PATHS || fs.readFileSync(PATHS_FILE, { encoding: 'utf-8' });
 }
