08c63b888dd2843fe78bf6e4f6514772f42af615
Andy Smith <andy.smith04@bbc.co.uk>
2017-07-25 09:18:09 +0100

    Ignore the iframe that gets injected into the head by Optimizely :unamused:

--- gen-a11y.js
+++ gen-a11y.js
@@ -26,7 +26,7 @@ function pathToOutput(baseUrl, path) {
   return `
     page("${url}", {
       ${setupSteps}
-      hide: ['orb', 'bbccookies-prompt']
+      hide: ['orb', 'bbccookies-prompt', '/html/head/iframe']
     })`;
 }
 
