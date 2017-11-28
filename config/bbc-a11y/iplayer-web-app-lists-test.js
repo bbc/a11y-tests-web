17edd0d42e338bbc840a5041db22bdb9ea4e052d
Andy Smith <andy.smith04@bbc.co.uk>
2017-11-28 13:31:45 +0000

    Add linting

--- config/bbc-a11y/iplayer-web-app-lists-test.js
+++ config/bbc-a11y/iplayer-web-app-lists-test.js
@@ -1,10 +1,12 @@
+'use strict';
+
 const { options } = require('./iplayer-web-common');
 
 module.exports = {
   options,
   baseUrl: 'https://frontdoor.iplayer.test.api.bbc.co.uk',
   paths: [
-    "/bbcone/a-z",
-    "/iplayer/most-popular"
+    '/bbcone/a-z',
+    '/iplayer/most-popular'
   ]
 };
