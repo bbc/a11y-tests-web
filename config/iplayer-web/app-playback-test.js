f343f378918964aec7e2c9d6c526330ec3b9bc0d
Sam Bason <sam.bason@bbc.co.uk>
2018-01-19 15:32:25 +0000

    Skip heading test for iPlayer Playback (#5)
    * Skip heading test for iPlayer Playback
    
    * Skip the carousel too because it is dying soon

--- config/iplayer-web/app-playback-test.js
+++ config/iplayer-web/app-playback-test.js
@@ -1,6 +1,19 @@
 'use strict';
 
-const { options } = require('./common');
+const { options: commonOptions } = require('./common');
+
+const commonSkips = commonOptions.skip;
+const playbackSpecificHides = ['carousel-outer-outer'];
+
+const commonHides = commonOptions.hide;
+const playbackSpecificSkips = ['Headings: Content must follow headings'];
+
+const options = Object.assign({}, commonOptions,
+  {
+    skip: [...commonSkips, ...playbackSpecificSkips],
+    hide: [...commonHides, ...playbackSpecificHides]
+  }
+);
 
 module.exports = {
   options,
