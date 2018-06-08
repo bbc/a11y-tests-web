ea501d29a48e787fd071132ca381eff83d90f99d
Andy Smith <contact@andymsmith.co.uk>
2018-06-08 14:43:38 +0100

    iPlayer config updates (#11)
    - Update to use test.bbc.co.uk by default for test configs
    - Add new paths to app-lists-test config

--- config/iplayer-web/app-lists-test.js
+++ config/iplayer-web/app-lists-test.js
@@ -7,6 +7,14 @@ module.exports = {
   baseUrl,
   paths: [
     '/bbcone/a-z',
-    '/iplayer/most-popular'
+    '/iplayer/most-popular',
+    '/iplayer/categories/arts/featured',
+    '/iplayer/categories/arts/a-z',
+    '/iplayer/categories/arts/most-recent',
+    '/iplayer/categories/drama-sci-fi-and-fantasy/featured',
+    '/iplayer/categories/drama-sci-fi-and-fantasy/a-z',
+    '/iplayer/categories/drama-sci-fi-and-fantasy/most-recent',
+    '/iplayer/search?q=east',
+    '/iplayer/episodes/b006m86d'
   ]
 };
