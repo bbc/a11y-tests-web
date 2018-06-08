f4db49724a2a001ee7c530aa6b01f9f9e55946e7
Andy Smith <andy.smith04@bbc.co.uk>
2018-06-08 13:00:07 +0100

    Skip resizable text test on iPlayer for now

--- config/iplayer-web/common.js
+++ config/iplayer-web/common.js
@@ -2,12 +2,15 @@
 
 const baseUrl = process.env.A11Y_IPLAYER_WEB_BASE_URL || 'https://www.bbc.co.uk';
 
+// Skipped tests are those for which we have tickets prioritised in the backlog to fix
+
 module.exports = {
   options: {
     hide: ['orb', 'bbccookies-prompt', '/html/head/iframe', 'smphtml5iframebbcMediaPlayer', 'tvip-channels-stream-inner'],
     skip: [
       'Title attributes: Title attributes only on inputs',
-      'Tables: Use tables for data'
+      'Tables: Use tables for data',
+      'Resizable text: Text must be styled with units that are resizable in all browsers'
     ]
   },
   baseUrl
