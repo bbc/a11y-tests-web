e71719afc815d77737b9cade4a257ca77d244223
Ben-Wormald <benwormald95@gmail.com>
2019-08-30 11:21:56 +0100

    add SMP element ignore

--- config/sounds-web/all.js
+++ config/sounds-web/all.js
@@ -2,7 +2,7 @@ const baseUrl = process.env.A11Y_SOUNDS_WEB_BASE_URL || 'https://www.bbc.co.uk';
 
 module.exports = {
   options: {
-    hide: ['orb', 'bbccookies-prompt', 'smphtml5iframebbcMediaPlayer', 'edr_l_first', 'bbcprivacy-prompt', 'id4-cta-', 'msi-modal', 'p_audioui_'],
+    hide: ['orb', 'bbccookies-prompt', 'smphtml5iframebbcMediaPlayer', 'smphtml5iframesmp-wrapper', 'edr_l_first', 'bbcprivacy-prompt', 'id4-cta-', 'msi-modal', 'p_audioui_'],
     skip: []
   },
   baseUrl,
