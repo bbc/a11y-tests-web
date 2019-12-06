63bfa82753b69433a5f4866f88dd665b9568acd0
Andy Smith <andy.smith04@bbc.co.uk>
2019-12-06 15:40:03 +0000

    Add lighthouse to CLI

--- cli.js
+++ cli.js
@@ -2,21 +2,22 @@
 const { spawn } = require('child_process');
 const yargs = require('yargs');
 const { clean: cleanBbcA11y, build: buildBbcA11y } = require('./lib/bbcA11y');
+const { run: runLighthouse } = require('./lib/lighthouse');
+const logger = require('./lib/colourfulLog');
 
-const argv = yargs
+yargs
   .usage('Usage: $0 <tool> [options] -- [tool options]')
-  .command('bbc-a11y', 'Runs bbc-a11y', runBbcA11y)
-  .command('lighthouse', 'Runs Google Lighthouse', runLighthouse)
+  .command('bbc-a11y', 'Runs bbc-a11y', runBbcA11yCli)
+  .command('lighthouse', 'Runs Google Lighthouse', runLighthouseCli)
   .demandCommand()
   .alias('h', 'help')
   .alias('m', 'mode')
   .describe('m', 'The mode to run the specified tool in')
   .choices('m', ['headless', 'junit', 'junit-headless', 'ci'])
+  .demandOption('m', 'You must specify a mode to run the tool in, e.g. junit-headless')
   .argv;
 
-console.log('my args', argv);
-
-async function runBbcA11y({ argv }) {
+async function runBbcA11yCli({ argv }) {
   buildBbcA11y();
 
   const { mode } = argv;
@@ -28,8 +29,18 @@ async function runBbcA11y({ argv }) {
   process.exit(bbcA11yExitCode);
 }
 
-function runLighthouse() {
-  console.log('lighthouse');
+function runLighthouseCli({ argv }) {
+  const { mode } = argv;
+  const validModes = ['junit', 'junit-headless'];
+
+  if (validModes.includes(mode)) {
+    if (mode === 'junit-headless') {
+      process.env.A11Y_HEADLESS = true;
+    }
+    return runLighthouse();
+  }
+
+  logger.error(`Unknown mode. Lighthouse is only supported in the following modes: ${validModes.join(', ')}`)
 }
 
 function getBbcA11yArgs(mode) {
