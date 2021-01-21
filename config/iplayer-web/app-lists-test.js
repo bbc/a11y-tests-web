e32207e4d1a1459bc0752c20a57b84c25143ffaa
Henry Warne <henry.warne@bbc.co.uk>
2021-01-21 15:00:22 +0000

    Update comments

--- config/iplayer-web/app-lists-test.js
+++ config/iplayer-web/app-lists-test.js
@@ -4,7 +4,7 @@ const { options: commonOptions, baseUrl } = require('./common');
 
 const commonSkips = commonOptions.skip;
 
-// Temporary fix for the dropdown component on the category pages
+// Temporarily skip test for the dropdown component on the category pages
 const listSpecificSkips = ['Forms: Managing focus: Forms must have submit buttons'];
 
 const options = Object.assign({}, commonOptions,
