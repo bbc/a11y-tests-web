822eadba5236da4d430b2507cccff049a0612928
Henry Warne <henry.warne@bbc.co.uk>
2021-01-21 14:57:53 +0000

    Add comment for dropdown specific skip

--- config/iplayer-web/app-homepage-test-signed-in.js
+++ config/iplayer-web/app-homepage-test-signed-in.js
@@ -4,6 +4,7 @@ const { options: commonOptions, baseUrl } = require('./common');
 
 const commonSkips = commonOptions.skip;
 
+// Temporary fix for the dropdown component on the category pages
 const listSpecificSkips = ['Forms: Managing focus: Forms must have submit buttons'];
 
 const options = Object.assign({}, commonOptions,
