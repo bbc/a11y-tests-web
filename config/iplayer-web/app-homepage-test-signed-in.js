7311a6e489079faae6bc34cc061d2cb8228cf580
Henry Warne <31985512+henrywarne@users.noreply.github.com>
2021-01-21 17:01:02 +0000

    IPLAYER-39705 - Add form submit button skip check on iPlayer web bundles and lists apps (#58)
    Co-authored-by: Daniel Barclay <barclaysd@me.com>

--- config/iplayer-web/app-homepage-test-signed-in.js
+++ config/iplayer-web/app-homepage-test-signed-in.js
@@ -1,6 +1,17 @@
 'use strict';
 
-const { options, baseUrl } = require('./common');
+const { options: commonOptions, baseUrl } = require('./common');
+
+const commonSkips = commonOptions.skip;
+
+// Temporarily skip test for the dropdown component on the category pages
+const listSpecificSkips = ['Forms: Managing focus: Forms must have submit buttons'];
+
+const options = Object.assign({}, commonOptions,
+  {
+    skip: [...commonSkips, ...listSpecificSkips]
+  }
+);
 
 module.exports = {
   options,
