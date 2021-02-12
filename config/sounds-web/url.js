be9a407657ef035b955320c8f95833ca11a5c040
Jenny Robertson <jenny.robertson@bbc.co.uk>
2021-02-12 13:39:00 +0000

    make url variable more specific

--- config/sounds-web/url.js
+++ config/sounds-web/url.js
@@ -4,9 +4,9 @@ const { options } = require('./common');
 
 module.exports = {
   options,
-  baseUrl: process.env.A11Y_SOUNDS_WEB_BASE_URL,
-  paths: [A11Y_SOUNDS_WEB_PATH]
+  baseUrl: process.env.A11Y_URL,
+  paths: []
 };
 
 // can be used to test single url passed in:
-// URL='http://www.google.com' A11Y_CONFIG=sounds-web/url a11y-tests-web bbc-a11y -m headless
+// A11Y_URL='http://www.google.com' A11Y_CONFIG=sounds-web/url a11y-tests-web bbc-a11y -m headless
