80371b4f3ead6557e3cbcbb6c82d42fa153ab04e
Jenny Robertson <jenny.robertson@bbc.co.uk>
2021-02-12 14:22:00 +0000

    Add single url command (#55)
    * and single url command

---
+++ config/sounds-web/url.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const { options } = require('./common');
+
+module.exports = {
+  options,
+  baseUrl: process.env.A11Y_URL,
+  paths: ['']
+};
+
+// can be used to test single url passed in:
+// A11Y_URL='http://www.google.com' A11Y_CONFIG=sounds-web/url a11y-tests-web bbc-a11y -m headless
