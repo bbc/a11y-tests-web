79b692e8303a1ebdf0963e29098d61f47316f604
James Franklin <120317465+JamesFranklin91@users.noreply.github.com>
2024-02-19 15:22:29 +0000

    updated paths for new myprogrammes paths (#66)

--- config/iplayer-web/app-myprogrammes-test.js
+++ config/iplayer-web/app-myprogrammes-test.js
@@ -8,11 +8,15 @@ module.exports = {
   paths: [
     '/iplayer/added',
     '/iplayer/recommendations',
-    '/iplayer/watching'
+    '/iplayer/watching',
+    '/iplayer/watchlist',
+    '/iplayer/continue-watching'
   ],
   signedInPaths: [
     '/iplayer/added',
     '/iplayer/recommendations',
-    '/iplayer/watching'
+    '/iplayer/watching',
+    '/iplayer/watchlist',
+    '/iplayer/continue-watching'
   ]
 };
